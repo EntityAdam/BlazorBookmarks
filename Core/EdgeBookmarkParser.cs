6b11d7e646bcb3b63209e5a60d0cf23a058f8f77
Adam Vincent <adam.vincent@ais.com>
2022-07-08 11:36:08 -0400

    begin parsing edge bookmarks export

--- Core/EdgeBookmarkParser.cs
+++ Core/EdgeBookmarkParser.cs
@@ -1,14 +1,41 @@
-﻿using System.IO;
+﻿using Core.Models;
+using HtmlAgilityPack;
+using System.IO;
 
 namespace Core
 {
     public class EdgeBookmarkParser
     {
-        private void ReadFile(string filename)
+        public (IEnumerable<FolderModel>, IEnumerable<BookmarkModel>) ReadFile(string filename)
         {
-            if (!Directory.Exists(filename))
+            if (!File.Exists(filename))
                 throw new FileNotFoundException();
-            var fileContents = File.ReadAllText(filename);
+            var doc = new HtmlDocument();
+            doc.Load(filename);
+            var bookmarkExportFolderNodes = doc.DocumentNode.SelectNodes("//h3");
+            var folderIndex = -1;
+            List<FolderModel> folders = new();
+            List<BookmarkModel> bookmarks = new();
+            foreach (var folderNode in bookmarkExportFolderNodes)
+            {
+                folderIndex++;
+                var folderName = folderNode.InnerText;
+                var addDate = folderNode.Attributes["ADD_DATE"].Value;
+                var lastModifiedInEpoch = folderNode.Attributes["LAST_MODIFIED"].Value;
+                var lastModifiedUtcDateTime = DateTimeOffset.FromUnixTimeSeconds(long.Parse(lastModifiedInEpoch)).UtcDateTime;
+                var folder = new FolderModel() { Id = folderIndex, Name = folderName, LastUpdated = lastModifiedUtcDateTime };
+                folders.Add(folder);
+            }
+            var bookmarkNodes = doc.DocumentNode.SelectNodes("//a");
+            foreach (var bookmarkNode in bookmarkNodes)
+            {
+                var bookMarkurl = bookmarkNode.Attributes["HREF"].Value;
+                var bookMarkaddDate = bookmarkNode.Attributes["ADD_DATE"].Value;
+                var bookMarkName = bookmarkNode.InnerText;
+                var bookmark = new BookmarkModel() { Name = bookMarkName, Url = bookMarkurl };
+                bookmarks.Add(bookmark);
+            }
+            return (folders, bookmarks);
         }
-    }    
+    }
 }
---
+++ UnitTests/EdgeParserTests.cs
@@ -0,0 +1,21 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests
+{
+    public class EdgeBookmarkParserTests
+    {
+        [Fact]
+        public void Test()
+        {
+            string filename = @"C:\test\multifolder.html";
+            var reader = new EdgeBookmarkParser();
+            var x = reader.ReadFile(filename);
+        }
+    }
+}
